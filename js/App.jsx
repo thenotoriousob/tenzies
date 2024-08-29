4bb7ccaf23a812af4c5cb2c89307192970684c32
thenotoriousob <32715357+thenotoriousob@users.noreply.github.com>
2024-08-29 11:32:25 +0100

    Add in games played
    Also use this to handle when a new interval is started
    Moved data relating to the game on its own line under the title

--- js/App.jsx
+++ js/App.jsx
@@ -1,13 +1,17 @@
-import React from "react"
+import React, {useState} from "react"
 import Die from "./components/Die"
 import {nanoid} from "nanoid"
 import Confetti from "react-confetti"
 
 export default function App() {
 
-    const [dice, setDice] = React.useState(allNewDice())
-    const [tenzies, setTenzies] = React.useState(false)
-    const [rolls, setRolls] = React.useState(0)
+    const [dice, setDice] = useState(allNewDice())
+    const [rolls, setRolls] = useState(0)
+    const [gamesPlayed, setGamesPlayed] = useState(1)
+    const [time, setTime] = useState(0)
+    const [intervalId, setIntervalId] = useState(0)
+    const [tenzies, setTenzies] = useState(false)
+    
     
     React.useEffect(() => {
         const allHeld = dice.every(die => die.isHeld)
@@ -15,8 +19,19 @@ export default function App() {
         const allSameValue = dice.every(die => die.value === firstValue)
         if (allHeld && allSameValue) {
             setTenzies(true)
+            clearInterval(intervalId)
         }
     }, [dice])
+
+    React.useEffect(() => {
+
+        setIntervalId(setInterval(() => {
+            setTime(prevTime => prevTime + 1)
+        }, 1000))
+
+        return () => clearInterval(intervalId)
+
+    },[gamesPlayed])
 
     function generateNewDie() {
         return {
@@ -45,6 +60,8 @@ export default function App() {
             setTenzies(false)
             setDice(allNewDice())
             setRolls(0)
+            setGamesPlayed(prevState => prevState + 1)
+            setTime(0)
         }
         setRolls(prevRolls => prevRolls + 1)
     }
@@ -69,7 +86,12 @@ export default function App() {
     return (
         <main>
             {tenzies && <Confetti />}
-            <h1 className="title">Tenzies <span>{rolls}</span></h1>
+            <h1 className="title">Tenzies</h1>
+            <div className="game-info">
+                <h2>Time {Math.floor(time / 60)}:{Math.floor(time % 60).toLocaleString('en-US', {minimumIntegerDigits: 2, useGrouping:false})}</h2>
+                <h2>Rolls {rolls}</h2>
+                <h2>Game number {gamesPlayed}</h2>
+            </div>
             <p className="instructions">Roll until all dice are the same. 
             Click each die to freeze it at its current value between rolls.</p>
             <div className="dice-container">
